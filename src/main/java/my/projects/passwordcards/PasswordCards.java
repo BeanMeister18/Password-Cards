3283745ed76000e6c4b8b314d3b2aa512b4c153b
Ben Boguslavsky <ben.boguslavsky@gmail.com>
2023-01-02 20:43:25 -0500

    adding main application class

---
+++ src/main/java/my/projects/passwordcards/PasswordCards.java
@@ -0,0 +1,11 @@
+package my.projects.passwordcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordCards {
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordCards.class, args);
+    }
+}

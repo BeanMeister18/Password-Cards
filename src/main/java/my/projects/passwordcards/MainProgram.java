4c549f7fba7bc8b0582c26e26c86afeaed7aedb5
Ben Boguslavsky <ben.boguslavsky@gmail.com>
2023-01-03 21:33:40 -0500

    renamed from 'PasswordCards'

--- src/main/java/my/projects/passwordcards/MainProgram.java
+++ src/main/java/my/projects/passwordcards/MainProgram.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PasswordCards {
+public class MainProgram {
     public static void main(String[] args) {
-        SpringApplication.run(PasswordCards.class, args);
+        SpringApplication.run(MainProgram.class, args);
     }
 }

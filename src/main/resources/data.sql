ede789fd6670d5dc192ac00090575792244d0461
Ben Boguslavsky <ben.boguslavsky@gmail.com>
2023-01-04 20:59:53 -0500

    DB initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `CREDENTIAL` (
+  id integer NOT NULL AUTO_INCREMENT,
+  website varchar(45) NOT NULL,
+  username varchar(45) NOT NULL,
+  password varchar(45) NOT NULL,
+  PRIMARY KEY (id)
+);

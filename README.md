a13216973fe5286c3ab3647ad8e4c91ad5f1cf6f
Ben Boguslavsky <ben.boguslavsky@gmail.com>
2023-01-05 23:06:10 -0500

    DB initialization

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Password-Cards
+
+Uses H2 in-memory database.
+
+DB console is on http://localhost:8080/h2-console
+
+Login credentials are in _application.properties_ file
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,11 @@
 CREATE TABLE `CREDENTIAL` (
   id integer NOT NULL AUTO_INCREMENT,
   website varchar(45) NOT NULL,
-  username varchar(45) NOT NULL,
+  username varchar(16) NOT NULL,
   password varchar(45) NOT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO CREDENTIAL (id, website, username, password) VALUES (1, 'site-1', 'user-1', 'psw-1');
+INSERT INTO CREDENTIAL (id, website, username, password) VALUES (2, 'site-2', 'user-2', 'psw-2');
+INSERT INTO CREDENTIAL (id, website, username, password) VALUES (3, 'site-3', 'user-3', 'psw-3');
